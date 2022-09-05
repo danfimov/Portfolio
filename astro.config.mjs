d4c2cf1a4c8053c68346c4fee2be192ac7996efa
Дмитрий Анфимов <danfimov@yandex-team.ru>
2022-09-05 18:55:36 +0300

    Deploy by vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [preact()],
+	"cleanUrls": true,
+	output: 'server',
+	adapter: vercel(),
 });
