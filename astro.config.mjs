3763cc19f5107cde56daa2264e53ecb3e30b1fd1
Дмитрий Анфимов <danfimov@yandex-team.ru>
2022-09-06 18:56:55 +0300

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,21 @@ import { defineConfig } from 'astro/config';
 
 import react from "@astrojs/react";
 
-// https://astro.build/config
-export default defineConfig({
-  integrations: [react()],
-  markdown: {
-    // Example: Switch to use prism for syntax highlighting in Markdown
-    syntaxHighlight:'shiki',
-  },
-  site: 'https://danfimov.github.io',
-  base: '/portfolio',
-});
+if (import.meta.env?.PORT === undefined) {
+  const env_port = 8080
+}
+else {
+  const env_port = Number(import.meta.env?.PORT)
+}
+
+export default defineConfig(
+  {
+    integrations: [react()],
+    markdown: {
+      // Example: Switch to use prism for syntax highlighting in Markdown
+      syntaxHighlight:'shiki',
+    },
+    output: 'static',
+    server: { port: import.meta.env?.PORT | 3000 }
+  }
+);
